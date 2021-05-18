c226de890ac3f1176fbedb2f27eb8aeefeac436d
colin oneill <coneill65@students.mchenry.edu>
2021-05-17 21:53:40 -0500

    added kotlin.

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     println("My name is colin ")
     println("I love programming and have been programming since I was 10.")
-    println("I dont have any experience with java.")
+    println("I dont have any experience with java, or kotlin.")
     println("My favorite programming language is python.")
 }
