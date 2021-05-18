e2001a075a7c45c7b2c89af64dc298110f828634
colin oneill <coneill65@students.mchenry.edu>
2021-05-17 21:51:47 -0500

    added another line.

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,5 @@ fun main() {
     println("My name is colin ")
     println("I love programming and have been programming since I was 10.")
     println("I dont have any experience with java.")
+    println("My favorite programming language is python.")
 }
