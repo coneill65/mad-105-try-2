46e275887347053e2bf3c83a5cada39a142277a9
colin oneill <coneill65@students.mchenry.edu>
2021-05-21 17:08:15 -0500

    vars.kt added for Variables lesson.

---
+++ src/vars.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val myByte: Byte = 1
+    val myShort: Short = 5
+    val myInt = 54
+    val myLong: Long = 3457869
+    val myFloat = 4.67F
+    val myDouble = 3.14159
+    val myBoolean = true
+    val myChar = 'C'
+
+    println("byte demo: $myByte")
+    println("short demo: $myShort")
+    println("int demo: $myInt")
+    println("long demo: $myLong")
+    println("float demo: $myFloat")
+    println("double demo: $myDouble")
+    println("boolean demo: $myBoolean")
+    println("char demo: $myChar")
+}
