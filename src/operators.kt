33d86759468977cf4b775089ecfdb95e16b0c65e
colin oneill <coneill65@students.mchenry.edu>
2021-05-21 20:09:48 -0500

    vars.kt added for Variables lesson.

---
+++ src/operators.kt
@@ -0,0 +1,16 @@
+fun main(){
+    print("45 + 23 = ")
+    println(45 + 23)
+
+    print("45 - 23 = ")
+    println(45 - 23)
+
+    print("45 * 23 = ")
+    println(45 * 23)
+
+    print("45 / 23 = ")
+    println(45 / 23)
+
+    print("45 % 23 = ")
+    println(45 % 23)
+}
