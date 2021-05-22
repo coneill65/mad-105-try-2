81fc9b5dfa2400be5aa4c22b107bedea84ef071a
colin oneill <coneill65@students.mchenry.edu>
2021-05-21 21:42:58 -0500

    when-statement.kt created and tested.

--- src/when-statement.kt
+++ src/when-statement.kt
@@ -2,16 +2,23 @@ fun main() {
     var input: Int?
 
     do {
-        print("Enter a number to be translated to german:")
+        print("Enter a number from 1-10 to be translated to german:")
         input  = readLine()!!.toIntOrNull()
         if (input == null) println("Not a valid number, try again")
     }
-    while (input == null)
+    while (input == null || input > 10 || input < 1)
 
 
     when {
-        input < 97.5 -> print("Temperature is low.")
-        input > 99.5 -> print("Temperature is high.")
-        else -> print("Temperature is normal.")
+        1 == input -> print("Einz")
+        2 == input -> print("Zwei")
+        3 == input -> print("Drei")
+        4 == input -> print("Vier")
+        5 == input -> print("Fünf")
+        6 == input -> print("Sechs")
+        7 == input -> print("Sieben")
+        8 == input -> print("Acht")
+        9 == input -> print("Neun")
+        10 == input -> print("Zehn")
     }
 }
